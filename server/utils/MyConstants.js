1d4c20285db69c13bf48e34121c4ea3bdb67ad9f
LycorisKyn <khang.pbhk652002@gmail.com>
2023-11-21 09:44:06 +0700

    server
    ShoppongOnline's server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.2xjttlb.mongodb.net',
+    DB_USER: 'adminkyn',
+    DB_PASS: 'mk06052002',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '0938543125',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'uniyuusha12345@gmail.com', // gmail service
+    EMAIL_PASS: 'lysnmixpomdbeclw'
+  };
+  module.exports = MyConstants;
